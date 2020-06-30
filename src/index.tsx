634a29ecb11da77fd64a5d3c4616f95fc9f5dc88
Gorgi Kosev <gorgi.kosev@gmail.com>
2020-06-30 12:21:15 +0100

    separating didn't work

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import { observable, computed, action } from 'mobx';
-import { observer } from 'mobx-react';
-import * as React from 'react';
-import * as RD from 'react-dom';
+import { observable, computed, action } from "mobx";
+import { observer } from "mobx-react";
+import * as React from "react";
+import * as RD from "react-dom";
+import { render } from "../dist/src.fc45d0fd";
 
 class Counter {
-  @observable name = 'A counter';
+  @observable name = "A counter";
   @observable value = 1;
   @action increment() {
     this.value += 1;
@@ -14,7 +15,7 @@ class Counter {
   }
 
   @computed get report() {
-    return this.name + ' = ' + this.value;
+    return this.name + " = " + this.value;
   }
 }
 
@@ -23,37 +24,32 @@ class AppState {
   @action add() {
     let c = new Counter();
     this.counterList.push(c);
-    c.name = 'Counter ' + this.counterList.length;
+    c.name = "Counter " + this.counterList.length;
   }
   @action removeLast() {
     this.counterList.pop();
   }
 
   @computed get sum() {
-    return this.counterList.map(c => c.value).reduce((a, b) => a + b, 0);
+    return this.counterList.map((c) => c.value).reduce((a, b) => a + b, 0);
   }
 
   @computed get report() {
-    return `Total counters ${this.counterList.length}, total sum ${this.sum}`
+    return `Total counters ${this.counterList.length}, total sum ${this.sum}`;
   }
 }
 
 @observer
 class AppView extends React.Component<{ app: AppState }> {
   render() {
-    console.log('Re-rendering app');
+    console.log("Re-rendering app");
     return (
       <div>
         <p>Welcome to the counter app</p>
-        {this.props.app.counterList.map(counter => (
-          <CounterView
-            key={counter.name}
-            counter={counter}
-          />
+        {this.props.app.counterList.map((counter) => (
+          <CounterView key={counter.name} counter={counter} />
         ))}
-        <p>
-          {this.props.app.report}
-        </p>
+        <CounterReport report={this.props.app.report} />
         <p>
           <button onClick={() => this.props.app.add()}>Add counter</button>
           <button onClick={() => this.props.app.removeLast()}>Remove counter</button>
@@ -64,17 +60,23 @@ class AppView extends React.Component<{ app: AppState }> {
 }
 
 @observer
-class CounterView extends React.Component<{counter: Counter}> {
+class CounterView extends React.Component<{ counter: Counter }> {
   render() {
     let c = this.props.counter;
-    console.log('Rendering individual counter', c.name);
+    console.log("Rendering individual counter", c.name);
     return (
       <div>
-        <span>{c.report}</span>{" "}
-        <button onClick={() => c.increment()}>Increment</button>
+        <span>{c.report}</span> <button onClick={() => c.increment()}>Increment</button>
       </div>
     );
   }
 }
 
-RD.render(<AppView app={new AppState()} />, document.getElementById('app'));
+@observer
+class CounterReport extends React.Component<{ report: string }> {
+  render() {
+    return <p>{this.props.report}</p>;
+  }
+}
+
+RD.render(<AppView app={new AppState()} />, document.getElementById("app"));
